27a2e80238ddc8a7968baa33c47fdfe3265e2faf
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-20 09:25:58 -0600

    Add cors"

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('./db/mongoose');
+const cors = require('cors');
 const userRouter = require('./routes/users');
 const movieRouter = require('./routes/movies');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(movieRouter);
