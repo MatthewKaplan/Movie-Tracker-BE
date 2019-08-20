5fc9349aa56e259b9acf2d3f18e603c90c1b5506
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-20 14:08:02 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('./db/mongoose');
+const cors = require('cors')
 const app = express();
 const userRouter = require('./routes/users');
 const movieRouter = require('./routes/movies');
+
+app.use(cors());
 
 app.use(express.json());
 app.use(userRouter);
