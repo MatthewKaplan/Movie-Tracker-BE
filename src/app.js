69c55d580a17477382460889d04efb04b99f3e1d
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-20 13:17:22 -0600

    Remove cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 require('./db/mongoose');
 const app = express();
-const cors = require('cors');
 const userRouter = require('./routes/users');
 const movieRouter = require('./routes/movies');
 
-app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(movieRouter);
