b0c5f9c909fb17e0c7ea9a8de2de6e5231791c66
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-20 07:22:25 -0600

    Create route so user can edit their info

--- src/routes/users.js
+++ src/routes/users.js
@@ -60,7 +60,7 @@ router.post('/users/logoutAll', auth, async (req, res) => {
 
 router.get('/users/me', auth, async (req, res) => {
 	res.send(req.user);
-})
+});
 
 router.get('/users/:id', async (req, res) => {
 	try {
@@ -74,12 +74,28 @@ router.get('/users/:id', async (req, res) => {
 	}
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = [ 'name', 'email', 'password' ];
+	const isValidUpdate = updates.every(update => allowedUpdates.includes(update));
+
+	if (!isValidUpdate) {
+		return res.status(400).send({ error: 'Invalid Updates!' });
+	}
+
 	try {
-		const user = await User.findByIdAndDelete(req.params.id);
-		if (!user) {
-			return res.status(404).send({ error: 'User could not be found' });
-		}
+		updates.forEach(update => (req.user[update] = req.body[update]));
+		await req.user.save();
+
+		res.send(req.user);
+	} catch (e) {
+		res.status(400).send();
+	}
+});
+
+router.delete('/users/me', auth, async (req, res) => {
+	try {
+		await req.user.remove();
 
 		res.send(user);
 	} catch (e) {
